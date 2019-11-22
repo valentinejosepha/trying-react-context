d820fd38d1e7f83d1db571d3275ca3c4249ea3f9
Valentine Voyen <valentine.voyen1@gmail.com>
2019-11-22 10:53:47 +0100

    using context this time

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React from 'react';
 import Toolbar from './components/Toolbar';
 import './App.css';
 
+//Context permit to add a prop in the component tree without
+// passing it explicitly in each components
+const ThemeContext = React.createContext('light');
 class App extends React.Component {
   render() {
-    return <Toolbar theme='dark' />;
+    // Use a Provider to pass theme lower in the tree
+    // Every component can read it, no matter where it is located in the tree
+    // Passing dark to the actual value
+    return(
+      <ThemeContext.Provider value='dark'>
+        <Toolbar />
+      </ThemeContext.Provider>
+    )
   }
 }
 
-function Toolbar(props) {
+// No need to transmit explicitly the theme
+function Toolbar = (props) => {
   return (
     <div>
-      <ThemedButton theme={props.theme} />
+      <ThemedButton />
     </div>
   );
 }
 
 class ThemedButton extends React.Component {
+  // Define a contextType to read the actual theme context
+  // React is gonna find the Provider of ancestral theme the most close and use its value
+  // Actual theme in this example = 'dark'
+  static contextType =ThemeContext;
   render () {
     return <Button theme={this.props.theme} />;
   }
 }
+
 export default App;
